530e189f8da996382e60acdd2c240d5a13033f32
Ryder <dev@ryderdamen.com>
2017-11-20 18:53:56 -0500

    Fixing clickability

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
+// ConversationViewer
+// Author: Ryder Damen (http://ryderdamen.com/conversation-viewer)
 
 var $ = jQuery;
-
 	
 function highlightPersonOnClick(className){
 	// When a name is clicked, highlight all members with the same class
 	
-	
 	// Highlight the clicked class	
 	$(className).click(function(e) {
-				
-		// Remove shadow from all elements
-		$(".CV-message").css( "box-shadow", "none" );
 		
-		// Place shadow on highlighted element
-		$(className).css( "box-shadow", "2px 2px 1px #888888" );
+		if ($(className).css("box-shadow") !=  "none" ) {
+			// If the element already has a box shadow, let's remove it with another click
+			$(className).css( "box-shadow", "none" );
+		}
+		else {
+			// Remove shadow from all elements
+			$(".CV-message").css( "box-shadow", "none" );
+		
+			// Place shadow on highlighted element
+			$(className).css( "box-shadow", "2px 2px 10px #888888" );	
+		}
 	});
-	
 	
 }
 
